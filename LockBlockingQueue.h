e7d28d37aac9362b71151088fd9a748e52376577
qqibrow <qqibrow@gmail.com>
2014-11-30 14:33:43 -0800

    separate from implementation.

--- LockBlockingQueue.h
+++ LockBlockingQueue.h
@@ -11,36 +11,16 @@ template <typename T>
 class LockBlockingQueue : public BlockingQueue<T> {
 public:
     LockBlockingQueue() : notEmpty_(false) {}
-    virtual T take() {
-        // First make sure queue is not empty.
-        std::unique_lock<std::mutex> lock(condition_mutex_);
-        while(!notEmpty_) {
-            condition_.wait(lock);
-        }
-
-        T curr = queue_.front();
-        queue_.pop();
-
-        lock.unlock();
-        condition_.notify_one();
-        return curr;
-    }
-
-    virtual void put(T value) {
-
-        {
-            std::lock_guard<std::mutex> lk(condition_mutex_);
-            queue_.push(value);
-            notEmpty_ = true;
-        }
-        condition_.notify_all();
-    }
-
+    virtual T take();
+    virtual void put(T value);
+    virtual ~LockBlockingQueue() {}
 private:
     std::queue<T> queue_;
     std::mutex condition_mutex_;
     std::condition_variable condition_;
     bool notEmpty_;
 };
+
+#include "LockBlockingQueue.cc"
 
 #endif
