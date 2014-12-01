6732c8f4cddf77a0d869da8ac25e53b8c8e0f313
qqibrow <qqibrow@gmail.com>
2014-11-30 23:55:04 -0800

    add gflags support.

--- main.cpp
+++ main.cpp
@@ -1,14 +1,19 @@
+#include <gflags/gflags.h>
 #include "LockBlockingQueue.h"
 #include "LockFreeBlockingQueue.h"
 #include "QueueTest.h"
 
+DEFINE_uint64(operator_count, 1000*1000, "");
 using namespace std;
 
-int main() {
-    long count = 1000*1000*10;
+
+int main(int argc, char* argv[]) {
+    gflags::ParseCommandLineFlags(&argc, &argv, true);
+
+    printf("Pass in operator count: %lld.\n", FLAGS_operator_count);
     QueueTest lockQueueTest(new LockBlockingQueue<long>());
-    lockQueueTest.RunTest(count, std::plus<long>());
+    lockQueueTest.RunTest(FLAGS_operator_count, std::plus<long>());
     QueueTest lockFreeQueueTest(new LockFreeBlockingQueue<long>());
-    lockFreeQueueTest.RunTest(count, std::plus<long>());
+    lockFreeQueueTest.RunTest(FLAGS_operator_count, std::plus<long>());
     return 0;
 }
