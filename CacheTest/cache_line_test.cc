4f395563490c594a916f08333cac9d719d95b76f
qqibrow <qqibrow@gmail.com>
2014-12-04 18:36:44 +0000

    cache_line_test.

--- CacheTest/cache_line_test.cc
+++ CacheTest/cache_line_test.cc
@@ -1,14 +1,20 @@
 #include <gflags/gflags.h>
+#include <chrono>
 
-//#define LENGTH  (64 * 1024 * 1024)
+#define LENGTH  (1024 * 1024 * 64)
 DEFINE_int32(KK, 1, "The step of running loop.");
-
+using namespace std;
+using namespace std::chrono;
 int main(int argc, char * argv[])
 {
-    int length = 1024*1024;
     gflags::ParseCommandLineFlags(&argc, &argv, true);
-    int arr[length] = {1};
+    int* arr = new int[LENGTH];
     const int K = FLAGS_KK;
-    for (int i = 0; i < length; i += K) arr[i] *= 3;
+    auto t0 = high_resolution_clock::now();
+    for (int i = 0; i < LENGTH; i += K) arr[i] *= 3;
+    auto t1 = high_resolution_clock::now();
+    nanoseconds total_nanoseconds = std::chrono::duration_cast<nanoseconds>(t1 - t0);
+    printf("Time exlipse %ld\n", total_nanoseconds.count());
+    delete[] arr;
     return 0;
 }
