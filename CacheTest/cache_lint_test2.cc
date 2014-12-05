3fc1d8bb0f98b8d442f0ac2feba3a272d146df7b
qqibrow <qqibrow@gmail.com>
2014-12-05 00:27:53 +0000

    Add second cache lint test program.

---
+++ CacheTest/cache_lint_test2.cc
@@ -0,0 +1,28 @@
+#include <thread>
+#include <chrono>
+
+using namespace std::chrono;
+using namespace std;
+
+#define MAX (1024 * 1024 * 32)
+int main(int argc, char const* argv[]) {
+    int x = 0;
+    //int array[1024];
+    int y = 0;
+
+    auto t0 = high_resolution_clock::now();
+    std::thread th([&x]() {
+        for (int i = 0; i < MAX; ++i) {
+            ++x;
+        }
+    });
+    for (int i = 0; i < MAX; ++i) {
+        ++y;
+    }
+
+    th.join();
+    auto t1 = high_resolution_clock::now();
+    nanoseconds total_nanoseconds = std::chrono::duration_cast<nanoseconds>(t1 - t0);
+    printf("Time exlipse %ld\n", total_nanoseconds.count());
+    return 0;
+}
