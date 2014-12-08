9ed79e79551c1772a436d13cbe7a43569950bf23
qqibrow <qqibrow@gmail.com>
2014-12-07 21:06:28 -0800

    Correct name of the files.

--- CacheTest/cache_line_test.cc
+++ CacheTest/cache_line_test.cc
@@ -1,17 +1,43 @@
-#include <gflags/gflags.h>
+#include <cstdio>
+#include <cstdlib>
 #include <chrono>
+#include <thread>
+#include <vector>
+#include <assert.h>
 
-#define LENGTH  (1024 * 1024 * 64)
-DEFINE_int32(KK, 1, "The step of running loop.");
 using namespace std;
 using namespace std::chrono;
+
+#define LENGTH  (1024 * 1024 * 32)
+
+void UpdateAtPosition(int* position) {
+    for(int i = 0; i < LENGTH; ++i) {
+        *position = *position + 3;
+    }
+}
 int main(int argc, char * argv[])
 {
-    gflags::ParseCommandLineFlags(&argc, &argv, true);
+    printf("%d", sizeof(int));
+    if(argc < 3) {
+        printf("Too few arguments\n");
+        return -1;
+    }
     int* arr = new int[LENGTH];
-    const int K = FLAGS_KK;
+
     auto t0 = high_resolution_clock::now();
-    for (int i = 0; i < LENGTH; i += K) arr[i] *= 3;
+
+    int num_thread = argc - 1;
+    std::vector<thread> threads;
+    for(int i = 0; i < num_thread; ++i) {
+        const int pos = atoi(argv[1 + i]);
+        assert(pos >= 0 && pos < LENGTH && "Pass in position must be in  the range");
+        threads.push_back(thread(UpdateAtPosition, arr + pos));
+    }
+
+    for(auto& th: threads) {
+        th.join();
+    }
+
     auto t1 = high_resolution_clock::now();
     nanoseconds total_nanoseconds = std::chrono::duration_cast<nanoseconds>(t1 - t0);
     printf("Time exlipse %ld\n", total_nanoseconds.count());
